dac33c3301ad06b1d3854251395fd117080da22e
LegendaryLHL <hoonlim123@gmail.com>
2022-12-15 13:02:34 +0800

    yes

---
+++ PixelToDraw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace roblox32bitpainter_starving_artist_
+{
+    public class PixelToDraw
+    {
+        public Color color { get; set; }
+        public Vector point { get; set; }
+        public PixelToDraw(Color colord, Vector pointd)
+        {
+            color = colord;
+            point = pointd;
+        }
+    }
+}
