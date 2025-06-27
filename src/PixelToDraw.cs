72934ef70ad6a12471eb6df89e437309c378b74e
Hoonlim Lee <hoonlimlee@gmail.com>
2025-06-27 16:28:09 +0800

    Cleanup (#2)
    * Cleanup files
    
    * Use cross platform libary
    
    * Better pauser
    
    * Fix not painting

---
+++ src/PixelToDraw.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace StarvingArtistScript
+{
+    public class PixelToDraw
+    {
+        public Rgb24 color { get; set; }
+        public Vector2 point { get; set; }
+        public PixelToDraw(Rgb24 color, Vector2 point)
+        {
+            this.color = color;
+            this.point = point;
+        }
+    }
+}
